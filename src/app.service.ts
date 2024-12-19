5f66e1f4ecf7ddc603467d0ee0a3018701df6c20
Hikaru Kobayashi <hikaru.kobayashi0719@gmail.com>
2024-12-19 22:10:37 +0900

    chore: apply format

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,11 +1,11 @@
-import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
 
 @Injectable()
 export class AppService {
   constructor(private configService: ConfigService) {}
 
   getHello(): string {
-    return this.configService.get<string>('DATABASE_USER');
+    return this.configService.get<string>("DATABASE_USER");
   }
 }
